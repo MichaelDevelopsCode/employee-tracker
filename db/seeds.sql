42296db92a8543eb9b07fc91ed242cbbf52afea3
Michael Arvelo <michaeldevelopscode@gmail.com>
2021-01-25 03:39:34 -0500

    added update, and async

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Art"), ("Finance"), ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", "$100,000", 1 ), ("Sales Person", "$80,000", 1), ("Lead Engineer", "$150,000", 2), ("Software Engineer", "$120,000", 2), ("Accountant", "$120,000", 4), ("Legal Team Lead", "$260,000", 5), ("Lawyer", "$200,000", 5);
+VALUES ("Sales Lead", "100000", 1 ), ("Sales Person", "80000", 1), ("Lead Engineer", "150000", 2), ("Software Engineer", "120000", 2), ("Accountant", "120000", 4), ("Legal Team Lead", "$260000", 5), ("Lawyer", "$200000", 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Micah", "Johnson", 1, null), ("Jone", "James", 1, 1);
--- server.js
+++ server.js
@@ -34,7 +34,8 @@ function showMenu() {
                 { name: "Add a department", value: "addDepartment" },
                 { name: "Add a role", value: "addRole" },
                 { name: "Add an employee", value: "addEmployee" },
-                { name: "Update an employee role", value: "updateRole" }
+                { name: "Update an employee role", value: "updateRole" },
+                { name: "Exit App", value: "exit" }
             ]
         }
     ]);
@@ -83,36 +84,35 @@ function viewEmployees() {
     );
 };
 
-function addDepartment() {
-    return inquirer.prompt([
+async function addDepartment() {
+    const response = await inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: "Enter department name:",
-            validate: nameInput => {
-                if (nameInput) {
+            validate: nameInput_1 => {
+                if (nameInput_1) {
                     return true;
                 } else {
                     return "Please enter department name";
                 }
             }
         }
-    ]).then(response => {
-            connection.query('INSERT INTO department (name) VALUES (?)', [response.name],
-                function(err, res) {
-                    if (err) throw err;
-                    console.log('Added department to ID: ' + res.insertId);
-                    // go back to menu
-                    startApp();
-                }
-            )
+    ]);
+    connection.query('INSERT INTO department (name) VALUES (?)', [response.name],
+        function (err, res) {
+            if (err)
+                throw err;
+            console.log('Added department to ID: ' + res.insertId);
+            // go back to menu
+            startApp();
         }
     );
 };
   
 
-function addRole() {
-    let departmentList = {};
+async function addRole() {
+    let departmentList = [];
     connection.query('SELECT * FROM department', 
         function(err, res) {
             if (err) throw err;
@@ -125,13 +125,13 @@ function addRole() {
             };
         }
     );
-    return inquirer.prompt([
+    const response = await inquirer.prompt([
         {
             type: 'input',
             name: 'title',
             message: "Enter title of role: ",
-            validate: titleInput => {
-                if (titleInput) {
+            validate: titleInput_1 => {
+                if (titleInput_1) {
                     return true;
                 } else {
                     return "Please enter title of role";
@@ -142,8 +142,8 @@ function addRole() {
             type: 'input',
             name: 'salary',
             message: "Enter role salary: ",
-            validate: salaryInput => {
-                if (salaryInput) {
+            validate: salaryInput_1 => {
+                if (salaryInput_1) {
                     return true;
                 } else {
                     return "Please enter role salary";
@@ -156,22 +156,132 @@ function addRole() {
             message: "Select department the role is in: ",
             choices: departmentList
         }
-    ]).then(response => {
-            const sql = `
-            department.name AS department
-            FROM role 
-            LEFT JOIN department 
-            ON role.department_id = department.id
+    ]);
+    connection.query(`INSERT INTO role (title, salary, department_id ) VALUES (?, ?, ?)`, [response.title, response.salary, response.department],
+        function (err_1, res_1) {
+            if (err_1)
+                throw err_1;
+            console.log('Added ' + response.title + ' to department ' + response.department);
+            // go back to menu
+            startApp();
+        }
+    );
+};
 
-            INSERT INTO role (title, salary, department_id ) VALUES (?, ?, ?)`
-            connection.query(`INSERT INTO role (title, salary, department ) VALUES (?, ?, ?)`, [response.title, response.salary, response.department],
-                function(err, res) {
-                    if (err) throw err;
-                    console.log('Added ' + res.title + ' to department ' + res.department);
-                    // go back to menu
-                    startApp();
+async function addEmployee() {
+    let roleList = [];
+    let managerList = [{name: "None", value: null}];
+    // get all roles in db and id to pass as value in table insertion
+    connection.query('SELECT * FROM role', 
+        function(err, res) {
+            if (err) throw err;
+            // get list of roles so user can choose from them
+            for (var i = 0; i < res.length; i++) {
+                roleList.push({
+                    name: res[i].title,
+                    value: res[i].id
+                });
+            };
+        }
+    );
+    // get all managers from db and grab id's to pass as value to table
+    connection.query('SELECT * FROM employee WHERE employee.manager_id IS NULL', 
+        function(err, res) {
+            if (err) throw err;
+            // get list of managers so user can choose from them
+            for (var i = 0; i < res.length; i++) {
+                managerList.push({
+                    name: (res[i].first_name + " " + res[i].last_name),
+                    value: res[i].id
+                });
+            };
+        }
+    );
+    const response = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "Enter the employee’s first name",
+            validate: firstInput_1 => {
+                if (firstInput_1) {
+                    return true;
+                } else {
+                    return "Please enter employee's first name";
                 }
-            )
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "Enter employee's last name",
+            validate: lastInput_1 => {
+                if (lastInput_1) {
+                    return true;
+                } else {
+                    return "Please employee's last name";
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Select the employee's role",
+            choices: roleList
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: "Select employee's manager if available",
+            choices: managerList
+        }
+    ]);
+    connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [response.first_name, response.last_name, response.role, response.manager],
+        function (err_2, res_2) {
+            if (err_2)
+                throw err_2;
+            console.log('Employee ' + response.first_name + ' ' + response.last_name + ' added');
+            // go back to menu
+            startApp();
+        }
+    );
+}
+
+async function updateRole(empList) {
+    let roleList = [];
+    // get all roles in db and id to pass as value in table insertion
+    connection.query('SELECT * FROM role', 
+        function (err, res) {
+            if (err) throw err;
+            // get list of roles so user can choose from them
+            for (var i = 0; i < res.length; i++) {
+                roleList.push({
+                    name: res[i].title,
+                    value: res[i].id
+                });
+            };
+        }
+    );
+
+    const response = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: "Select employee to edit: ",
+            choices: empList
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Select the employee's new role",
+            choices: roleList
+        }
+    ]);
+    connection.query(`UPDATE employee SET role_id = ? WHERE employee.id = ?`, [response.role, response.employee],
+        function (err_2, res_2) {
+            if (err_2) throw err_2;
+            console.log('Employee role updated');
+            // go back to menu
+            startApp();
         }
     );
 }
@@ -197,10 +307,30 @@ const startApp = async () => {
                 return addRole();
             }
             if (response.mainMenu === "addEmployee") {
-
+                return addEmployee();
             }
             if (response.mainMenu === "updateRole") {
-
+                // get all roles in db and id to pass as value in table insertion
+                connection.promise().query('SELECT * FROM employee')
+                .then((rows,field) => {
+                    let empList = [];
+                    // get list of roles so user can choose from them
+                    for (var i = 0; i < rows[0].length; i++) {
+                        empList.push({
+                            name: (rows[0][i].first_name + ' ' + rows[0][i].last_name),
+                            value: rows[0][i].id
+                        });
+                    };
+                    console.log(empList);
+                    return updateRole(empList);
+                })
             }
+            if (response.mainMenu === "exit") {
+                console.log("Bye!");
+                return process.exit();
+            }
+        })
+        .catch(err => {
+            console.log(err);
         });
 }
